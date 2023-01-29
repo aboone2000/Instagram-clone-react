ad1db0d63ea976ffc305d0bc1424c7303a194b17
aboone2000 <aboone2000@gmail.com>
2023-01-29 15:04:12 -0500

    Updated App.js File

---
+++ instagram-clone/src/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="app-header">
+        <img
+        className="app_headerImage"
+        src=""
+        alt=""
+        />
+
+
+      </div>
+
+      <h1>Let's Build a Instagram Clone with React</h1>
+
+      {/*Posts */}
+      {/*Posts */}
+
+
+    </div>
+  );
+}
+
+export default App;
